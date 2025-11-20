37026afd0bfc5d412830632ba571c910be91009a
Irene Birtum <iic@birtum.com>
2025-11-20 12:24:38 -0300

    migraiton

--- account_cashbox_l10n_latam_check/__manifest__.py
+++ account_cashbox_l10n_latam_check/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Check Cashbox management",
     "summary": "Add cashbox for check operations",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
